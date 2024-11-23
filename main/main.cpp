eda48784a34523afa911f045be97d2ceb7c3da5b
Scaxlibur <scaxlibur@gmail.com>
2024-11-23 20:58:28 +0800

    新增WiFi模块

--- main/main.cpp
+++ main/main.cpp
@@ -2,8 +2,8 @@
 #include "fingerID.hpp"
 #include "screen.hpp"
 #include "servo.cpp"
-    
-
+#include "wifi.cpp"
+#include "mqtt.c"
                   
 /*
 void servotask(void *arg)
@@ -19,6 +19,26 @@ void servotask(void *arg)
 }
 */
 
+void WIFItask(void *arg)
+{
+    /* 初始化非易失性存储库 (NVS) */
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+    printf("ESP_WIFI_MODE_STA \n");
+    wifi_init_sta();
+    while(1) vTaskDelay(10 / portTICK_PERIOD_MS);
+}
+
+void MQTTtask(void *arg)
+{
+    void mqtt_app_start(void);
+    while(1) vTaskDelay(10 / portTICK_PERIOD_MS);
+}
+
 void IDtask(void *arg)
 {
     IDENTIFIER identifier;      //创建指纹识别器对象    
@@ -31,12 +51,14 @@ void IDtask(void *arg)
         printf("\nIDtask任务调用\n");
         #endif
         vTaskDelay(5000/portTICK_PERIOD_MS);
-    }
+    }    
 }
 
 extern "C" void app_main(void)
 {
     printf("power on\n");
     //xTaskCreate(servotask, "servotask", 12 * 1024, NULL, 1, NULL);
-    xTaskCreate(IDtask, "idtask", 12 * 1024, NULL, 1, NULL);
+    //xTaskCreate(IDtask, "idtask", 12 * 1024, NULL, 1, NULL);
+    xTaskCreate(WIFItask, "wifitask", 12 * 1024, NULL, 1, NULL);
+    //xTaskCreate(MQTTtask, "mqtttask", 12 * 1024, NULL, 1, NULL);
 }
---
+++ main/wifi.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "esp_system.h"
+#include "spi_flash_mmap.h"
+#include "esp_wifi.h"
+#include "driver/gpio.h"
+#include "nvs_flash.h"
+#include "esp_smartconfig.h"
+
+/* 宏定义WiFi名称和密码 */
+#define MY_WIFI_SSID    "ESP32YYDS"
+#define MY_WIFI_PASSWD  "ESP32YYDS"
+
+/* 宏定义WiFi连接事件标志位、连接失败标志位及智能配网标志位 */
+#define WIFI_CONNECTED_BIT  BIT0
+#define WIFI_FAIL_BIT       BIT1
+#define SMART_CONFIG_BIT    BIT2
+
+/* 定义一个WiFi连接事件标志组句柄 */
+static EventGroupHandle_t wifi_event_group_handler;
+
+/* 系统事件循环处理函数 */
+static void event_handler(void* arg, esp_event_base_t event_base,int32_t event_id, void* event_data);
+
+static void smartconfig_init_start(void);
+
+static void wifi_init_sta(void);
